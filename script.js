1e77d8328fa4f22c4e15f0e490875dfe32bb11f1
Guru Prasad G <128472613+GuruPrasad403@users.noreply.github.com>
2023-11-11 22:44:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+//Variables and constants
+
+const n = document.querySelector(".check");
+var message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highscore");
+const number = document.querySelector(".number");
+const reload = document.querySelector(".again")
+let scoreValue =20;
+let Highscorevalue = 0;
+
+// random Number genrating
+const scrceatNumber = Math.floor((Math.random())*21);
+
+// Event listeners
+
+n.addEventListener("click", function (){
+const num = Number(document.querySelector(".guess").value);
+
+console.log(num)
+if(!num){
+message.innerHTML="please enter a number";
+}
+else if(num == scrceatNumber){
+    document.querySelector("body").style.backgroundColor="green"
+    setTimeout(() =>{
+        document.querySelector("body").style.backgroundColor="#222"
+    },1000)
+    number.innerHTML=scrceatNumber;
+    message.innerHTML="Grate, it's a correct Number";
+    Highscorevalue++;
+    highScore.innerHTML=Highscorevalue
+}else if(num>scrceatNumber ||  num < scrceatNumber){
+    scoreValue--;
+    score.innerHTML= scoreValue;
+    message.textContent="Worng Number";
+    if(scoreValue==0){
+    message.textContent="Game Over";
+    n.disabled=true;
+    setTimeout(() =>{
+        document.querySelector("body").style.backgroundColor="#f00"
+    },100)
+}
+}  
+})
+
+// to reload a page
+reload.addEventListener("click", ()=>{
+    location.reload();
+    
+})
+
